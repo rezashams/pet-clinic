1e93b5d88a87852bdf8f6e101a8b5ca32133422a
Reza Shams <rezashams86@gmail.com>
2021-05-26 16:51:12 +0430

    added PetTypes too loading Data

--- pet-clinic-web/src/main/java/com/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/petclinic/bootstrap/DataLoader.java
@@ -1,11 +1,11 @@
 package com.petclinic.bootstrap;
 
 import com.petclinic.model.Owner;
+import com.petclinic.model.PetType;
 import com.petclinic.model.Vet;
 import com.petclinic.services.OwnerService;
+import com.petclinic.services.PetTypeService;
 import com.petclinic.services.VetService;
-import com.petclinic.services.map.OwnerServiceMap;
-import com.petclinic.services.map.VetServiceMap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -14,14 +14,25 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        dog.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Reza");
         owner1.setLastName("Shams");
