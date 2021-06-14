1fd7cf570993d3e407500f5aa8de2b36d35af8e6
Reza Shams <rezashams86@gmail.com>
2021-06-14 09:07:38 +0430

    implement owner service

--- pet-clinic-data/src/main/java/com/petclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/petclinic/repositories/OwnerRepository.java
@@ -4,4 +4,7 @@ import com.petclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner,Long> {
+
+    Owner findByLastName(String lastName);
+
 }
