61ac04a6b75f3ef71f8554f51b4340ab26e6ab92
Reza Shams <rezashams86@gmail.com>
2021-06-14 11:01:57 +0430

    Update profiles config

--- pet-clinic-data/src/main/java/com/petclinic/services/map/SpecialtyMapService.java
+++ pet-clinic-data/src/main/java/com/petclinic/services/map/SpecialtyMapService.java
@@ -2,11 +2,13 @@ package com.petclinic.services.map;
 
 import com.petclinic.model.Speciality;
 import com.petclinic.services.SpecialtyService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@Profile({"default", "map"})
 public class SpecialtyMapService extends AbstractMapService<Speciality, Long> implements SpecialtyService {
 
     @Override
