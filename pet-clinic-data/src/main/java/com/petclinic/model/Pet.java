2b4b7d51e02870052af8162993a84e06a417924e
Reza Shams <rezashams86@gmail.com>
2021-07-01 12:40:01 +0430

    Add new Visit features to owner page

--- pet-clinic-data/src/main/java/com/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/petclinic/model/Pet.java
@@ -4,16 +4,29 @@ import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
 import java.util.Set;
 
 @Setter
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 @Entity
 @Table(name = "pets")
 public class Pet extends BaseEntity {
+
+    @Builder
+    public Pet(Long id, String name, PetType petType, Owner owner, LocalDate birthDate, Set<Visit> visits) {
+        super(id);
+        this.name = name;
+        this.petType = petType;
+        this.owner = owner;
+        this.birthDate = birthDate;
+
+        if (visits == null || visits.size() > 0 ) {
+            this.visits = visits;
+        }
+    }
 
     @Column(name="name")
     private String name;
@@ -30,6 +43,6 @@ public class Pet extends BaseEntity {
     private LocalDate birthDate;
 
     @OneToMany(cascade = CascadeType.ALL,mappedBy = "pet")
-    private Set<Visit>visits;
+    private Set<Visit>visits = new HashSet<>();
 
 }
